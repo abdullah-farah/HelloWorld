5d20eee34e6d27f2195f1d5a2d9794476c674761
Abdullah Al Farah (Shawon) <al.farah@bjitgroup.com>
2017-04-04 09:14:25 +0600

    [Hw]: test Commit
    - test commit done.

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Shawon on 4/4/17.
+//  Copyright © 2017 BJIT Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Shawon on 4/4/17.
+//  Copyright © 2017 BJIT Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
